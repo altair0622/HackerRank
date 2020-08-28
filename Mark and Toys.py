a5ce38b2e4ece12948d2eb9371e4346809224364
Han Kim <51448660+altair0622@users.noreply.github.com>
2020-08-28 16:45:13 -0400

    Update Mark and Toys.py

--- Mark and Toys.py
+++ Mark and Toys.py
@@ -7,8 +7,34 @@ import re
 import sys
 
 # Complete the maximumToys function below.
+def merge_sort(values): 
+  
+    if len(values)>1: 
+        m = len(values)//2
+        left = values[:m] 
+        right = values[m:] 
+        left = merge_sort(left) 
+        right = merge_sort(right) 
+  
+        values =[] 
+  
+        while len(left)>0 and len(right)>0: 
+            if left[0]<right[0]: 
+                values.append(left[0]) 
+                left.pop(0) 
+            else: 
+                values.append(right[0]) 
+                right.pop(0) 
+  
+        for i in left: 
+            values.append(i) 
+        for i in right: 
+            values.append(i) 
+                  
+    return values
+
 def maximumToys(arr, k):
-    arr.sort()
+    arr = merge_sort(arr) # replace with arr.sort()
     
     count = 0
     for ele in arr:
