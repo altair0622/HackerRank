5da335a5d6abea36f92549ad8711d61d8bac544e
Han Kim <51448660+altair0622@users.noreply.github.com>
2020-09-11 12:29:54 -0400

    Update Symmetric Pairs.sql

--- Symmetric Pairs.sql
+++ Symmetric Pairs.sql
@@ -4,5 +4,6 @@ inner join functions as f2
 on f1.x = f2.y and f1.y = f2.x
 
 group by f1.x, f1.y
-having count(f1.x)>1 or f1.x < f1.y
+having count(f1.x)>1 -- or count(f1.y)>1
+or f1.x < f1.y
 order by f1.x asc
