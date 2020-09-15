3b7bbc4294d714592330b07c58fb4f053a66a60c
Han Kim <51448660+altair0622@users.noreply.github.com>
2020-09-15 17:14:37 -0400

    Update SQL Project Planning.sql

--- SQL Project Planning.sql
+++ SQL Project Planning.sql
@@ -1,5 +1,13 @@
-select A.start_date, count(B.end_date)
-from projects A
-inner join projects B
-on A.end_date = B.start_date
+select A.start_date, min(B.end_date)
+from 
+(select start_date 
+from projects 
+where start_date not in (select end_date from projects)) A,
+
+(select end_date 
+from projects 
+where end_date not in (select start_date from projects)) B
+
+where A.start_date < B.end_date
 group by A.start_date
+order by datediff(A.start_date, min(B.end_date)) desc, A.start_date
